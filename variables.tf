aa25f0642e6130d8bd8f929677cca0b1412e6d70
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-20 22:24:24 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,11 +2,7 @@ variable "client_id" {}
 variable "client_secret" {}
 
 variable "agent_count" {
-    default = 3
-}
-
-variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+    default = 2
 }
 
 variable "dns_prefix" {
@@ -23,18 +19,4 @@ variable resource_group_name {
 
 variable location {
     default = "West Europe"
-}
-
-variable log_analytics_workspace_name {
-    default = "testLogAnalyticsWorkspaceName"
-}
-
-# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
-variable log_analytics_workspace_location {
-    default = "westeurope"
-}
-
-# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing 
-variable log_analytics_workspace_sku {
-    default = "PerGB2018"
 }
