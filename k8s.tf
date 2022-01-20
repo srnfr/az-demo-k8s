f0251d278b6a032bca0ea0c7f2dae076e687203a
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-20 22:23:21 +0100

    Update k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -3,69 +3,43 @@ resource "azurerm_resource_group" "k8s" {
     location = var.location
 }
 
-resource "random_id" "log_analytics_workspace_name_suffix" {
-    byte_length = 8
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
 }
 
-resource "azurerm_log_analytics_workspace" "test" {
-    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
-    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
-    location            = var.log_analytics_workspace_location
-    resource_group_name = azurerm_resource_group.k8s.name
-    sku                 = var.log_analytics_workspace_sku
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.1.0.0/16"]
 }
 
-resource "azurerm_log_analytics_solution" "test" {
-    solution_name         = "ContainerInsights"
-    location              = azurerm_log_analytics_workspace.test.location
-    resource_group_name   = azurerm_resource_group.k8s.name
-    workspace_resource_id = azurerm_log_analytics_workspace.test.id
-    workspace_name        = azurerm_log_analytics_workspace.test.name
-
-    plan {
-        publisher = "Microsoft"
-        product   = "OMSGallery/ContainerInsights"
-    }
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+  address_prefixes     = ["10.1.0.0/22"]
 }
 
-resource "azurerm_kubernetes_cluster" "k8s" {
+resource "azurerm_kubernetes_cluster" "example" {
     name                = var.cluster_name
     location            = azurerm_resource_group.k8s.location
     resource_group_name = azurerm_resource_group.k8s.name
     dns_prefix          = var.dns_prefix
 
-    linux_profile {
-        admin_username = "ubuntu"
-
-        ssh_key {
-            key_data = file(var.ssh_public_key)
-        }
-    }
 
     default_node_pool {
         name            = "agentpool"
         node_count      = var.agent_count
         vm_size         = "Standard_D2_v2"
+        vnet_subnet_id = azurerm_subnet.internal.id
     }
 
-    service_principal {
-        client_id     = var.client_id
-        client_secret = var.client_secret
-    }
-
-    addon_profile {
-        oms_agent {
-        enabled                    = true
-        log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
-        }
-    }
 
     network_profile {
         load_balancer_sku = "Standard"
-        network_plugin = "kubenet"
-    }
-
-    tags = {
-        Environment = "Development"
+        network_plugin = "azure"
+        network_policy = "calico"
     }
 }
