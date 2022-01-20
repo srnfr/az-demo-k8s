6c6f01a5df5ceca9000fb1e60e710cf379d5d7c7
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-20 22:46:40 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -12,6 +12,13 @@ variable "client_secret" {}
 
 provider "azurerm" {
   features {}
+  
+  subscription_id = var.sub_id
+  client_id       =  var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+
+  
 }
 resource "azurerm_resource_group" "rg" {
   name = "training-demo-srn"
