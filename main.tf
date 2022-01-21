1ced3d09a46f8e301ccb0dfc2690ca7baf40a585
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-21 19:43:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,14 +26,14 @@ resource "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
   location            = data.azurerm_resource_group.example.location
   resource_group_name = data.azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/8"]
+  address_space       = ["10.244.0.0/16"]
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   virtual_network_name = azurerm_virtual_network.example.name
   resource_group_name  = data.azurerm_resource_group.example.name
-  address_prefixes     = ["10.244.0.0/16"]
+  address_prefixes     = ["10.244.1.0/24"]
 }
 
 resource "azurerm_kubernetes_cluster" "example" {
@@ -42,7 +42,13 @@ resource "azurerm_kubernetes_cluster" "example" {
     resource_group_name = data.azurerm_resource_group.example.name
     dns_prefix          = var.prefix
 
-
+    linux_profile {
+      admin_username = "ubuntu"
+      ssh_key {
+        key_data = file(var.ssh_public_key)
+      }
+    }
+  
     default_node_pool {
         name            = "defaultpool"
         node_count      = var.node_count
