2096b56c45fe4a072612124a974b99fec412fba8
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-21 19:48:37 +0100

    Update data-sources.tf

--- data-sources.tf
+++ data-sources.tf
@@ -3,3 +3,8 @@ data "azurerm_resource_group" "example" {
   ## DATA block because already exists and managed externally
   name     = "${var.resource_group_name}"
 }
+
+data "azurerm_ssh_public_key" "example" {
+  name                = "srn"
+  resource_group_name = "training-demo-srn"
+}
