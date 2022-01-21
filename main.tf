1af91bbf0d88407462bef114c7614753742a07f2
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-01-21 04:57:03 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "prefix" {
+  type        = string
+}
+
+variable "cluster_name" {
+  type        = string
+}
+
+variable "agent_count" {
+  type        = string
+}
+
+#####
+
 resource "azurerm_resource_group" "example" {
     name     = var.resource_group_name
     location = var.location
