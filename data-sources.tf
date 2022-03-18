57727f0c9a9e0357e0b2c589a00347e8eb12367c
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-03-18 13:15:26 +0100

    Update data-sources.tf

--- data-sources.tf
+++ data-sources.tf
@@ -10,7 +10,7 @@ data "azurerm_ssh_public_key" "example" {
 }
 
 data "azurerm_resources" "example" {
-  resource_group_name = "${var.resource_group_name}"
+  resource_group_name = "mc_training-demo-srn_k8s-az_westeurope"
   type = "Microsoft.Network/networkSecurityGroups"
 }
 
