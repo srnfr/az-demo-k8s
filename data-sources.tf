6cdb1988f1d32265b81c0c9eb6824569d4dfb19f
Stephane REYTAN <srnfr@users.noreply.github.com>
2022-03-20 15:59:27 +0100

    Update data-sources.tf

--- data-sources.tf
+++ data-sources.tf
@@ -8,11 +8,10 @@ data "azurerm_ssh_public_key" "example" {
   name                = "${var.ssh_key_name}"
   resource_group_name = "${var.resource_group_name}"
 }
-
-data "azurerm_resources" "example" {
-  resource_group_name = "${var.rg_nsg}"
-  type = "Microsoft.Network/networkSecurityGroups"
-}
+#data "azurerm_resources" "example" {
+#  resource_group_name = "${var.rg_nsg}"
+#  type = "Microsoft.Network/networkSecurityGroups"
+#}
 
 #output name_nsg {
 #    value = data.azurerm_resources.example.resources.0.name
